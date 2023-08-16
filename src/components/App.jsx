200ea53aee1f0f78663e38fe2bf22b5bbd5edfe7
Andrii Chychkan <mywork1728@gmail>
2023-08-16 21:22:52 +0300

    added modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Button } from './Button/Button';
+import { Gallery } from './ImageGallery/ImageGallery';
+import { GalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import { Searchbar } from './Searchbar/Seatchbar';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Searchbar />
+      <Gallery />
+      <Loader />
+      <Button />
+      <Modal />
+      <GalleryItem />
+    </>
   );
 };
